ec12f251b8acd40013ba145833b198695ee152d0
Sumaiya Afrin <sumaiya.afrin1008@gmail.com>
2024-12-22 23:09:23 +0600

    added footer

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../shared/Navbar";
+import Footer from "../shared/Footer";
 
 const MainLayout = () => {
   return (
     <div>
       <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 };
